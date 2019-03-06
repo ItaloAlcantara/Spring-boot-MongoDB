3d3ce6e2443af00805712b0abd3b4d3d1c948a58
Italo Jose <italo.ads31@gmail.com>
2019-03-06 14:14:19 -0300

    Find user by Id and Create error message custom

--- src/main/java/com/italojose/spring/Service/UserServices.java
+++ src/main/java/com/italojose/spring/Service/UserServices.java
@@ -1,11 +1,13 @@
 package com.italojose.spring.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.italojose.spring.Repository.UserRepository;
+import com.italojose.spring.Service.exception.ObjectNotFoundException;
 import com.italojose.spring.domain.User;
 
 @Service
@@ -16,5 +18,10 @@ public class UserServices {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id){
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("User not found"));
 	}
 }
--- src/main/java/com/italojose/spring/resources/UserResource.java
+++ src/main/java/com/italojose/spring/resources/UserResource.java
@@ -1,11 +1,11 @@
 package com.italojose.spring.resources;
 
-
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,16 +17,22 @@ import com.italojose.spring.dto.UserDTO;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserServices service;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<UserDTO>> findAll(){
-		
-		List<User> list =service.findAll();
+	public ResponseEntity<List<UserDTO>> findAll() {
+
+		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-				
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findUserById(@PathVariable String id) {
+		
+		User user = service.findById(id);	
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
