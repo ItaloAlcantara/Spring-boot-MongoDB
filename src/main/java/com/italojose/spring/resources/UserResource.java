7b146863f9c59c7e828bbb954cb1fad0840c588e
Italo Jose <italo.ads31@gmail.com>
2019-03-06 11:32:43 -0300

    Create UserDTO and Refatored UserResource

--- src/main/java/com/italojose/spring/resources/UserResource.java
+++ src/main/java/com/italojose/spring/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.italojose.spring.resources;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.italojose.spring.Service.UserServices;
 import com.italojose.spring.domain.User;
+import com.italojose.spring.dto.UserDTO;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,9 +22,11 @@ public class UserResource {
 	private UserServices service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		
 		List<User> list =service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+				
+		return ResponseEntity.ok().body(listDto);
 	}
 }
